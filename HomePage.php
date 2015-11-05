73626a6119abbeb54a1ab260ae35a55aaad2aa15
Roronoa <wld-ice@hotamil.com>
2015-11-05 23:15:00 +0000

    5/11/2015

---
+++ HomePage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link rel="stylesheet" type="text/css" href="DarkGtr2.css">
+</head>
+<body>
+<div ID = "MainHeader">
+    <div class="Container">
+        <div ID="LogoArea"></div>
+        <div ID="NavArea">
+            <ul ID="Nav">
+                <a href="HomePage.php"><li>Home</li></a>
+            </ul>
+        </div>
+    </div>
+</div>
+<div ID="MainArea">
+    <div class="Container page">
+        If user and password is correct this page will display his/her username and access_level
+    </div>
+</div>
+<div ID="footer">
+    <div class="Container">
+        Contact: 1400003@rgu.ac.uk
+    </div>
+</div>
+</body>
---
+++ Login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link rel="stylesheet" type="text/css" href="DarkGtr2.css">
+</head>
+<body>
+<div ID = "MainHeader">
+    <div class="Container">
+        <div ID="LogoArea"></div>
+        <div ID="NavArea">
+            <ul ID="Nav">
+                <a href="index.html"><li>Home</li></a>
+            </ul>
+        </div>
+    </div>
+</div>
+<div ID="MainArea">
+    <div class="Container page">
+        Display username and password for a user with a check login!
+    </div>
+</div>
+<div ID="footer">
+    <div class="Container">
+        Contact: 1400003@rgu.ac.uk
+    </div>
+</div>
+</body>

fce49a3db9870f654e3e34c6f4857ab695a1384c
Roronoa <wld-ice@hotamil.com>
2015-11-05 23:21:27 +0000

    5/11/2015

---
+++ Register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link rel="stylesheet" type="text/css" href="DarkGtr2.css">
+</head>
+<body>
+<div ID = "MainHeader">
+    <div class="Container">
+        <div ID="LogoArea"></div>
+        <div ID="NavArea">
+            <ul ID="Nav">
+                <a href="Login.php"><li>Login</li></a>
+                <a href="index.html"><li>Home</li></a>
+            </ul>
+        </div>
+    </div>
+</div>
+<div ID="MainArea">
+    <div class="Container page">
+        <?php
+
+        ?>
+    </div>
+</div>
+<div ID="footer">
+    <div class="Container">
+        Contact: 1400003@rgu.ac.uk
+    </div>
+</div>
+</body>

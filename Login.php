f72976f96ad05ebcec22c59654ddbb5b7379a9c5
1400003 <1400003@rgu.ac.uk>
2015-11-09 13:27:50 +0000

    Mike test 1

--- Login.php
+++ Login.php
@@ -1,22 +1,3 @@
-<?php
-    require('config.php');
-
-    if (isset($_POST['Login'])){
-        include('$db');
-
-    $username = mysql_escape_string($_POST['username']);
-    $password = mysql_escape_string($_POST['password']);
-    $password = md5($password);
-    $sqlget = mysql_query ("SELECT * FROM users WHERE Username= '$username'
-    AND Password = '$password'");
-
-    if (mysql_num_rows($sqlget) > 0) {
-        echo "you are now logged in.";
-    } else {
-        echo "wrong combo";
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -49,6 +30,24 @@
                     <input type="password" name="password">
                     <input type="checkbox" name="remember me">remember me<br>
                     <input ID="LoginButton" type="submit" name="Login" value="Log in">
+                <?php
+                    require('config.php');
+
+                    if (isset($_POST['Login'])){
+                        include('$db');
+
+                        $username = mysql_escape_string($_POST['username']);
+                        $password = mysql_escape_string($_POST['password']);
+                        $password = md5($password);
+                        $sqlget = mysql_query ("SELECT * FROM users WHERE Username= '$username'
+                        AND Password = '$password'");
+                        if (mysql_num_rows($sqlget) > 0) {
+                            echo "you are now logged in.";
+                        } else {
+                            echo "wrong combo";
+                        }
+                                                }
+                ?>
             </form>
         </div>
     </div>

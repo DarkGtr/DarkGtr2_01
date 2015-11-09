1f0a8a77d3fa568cbf1b35f76c4ca6f9e71ccc14
1400003 <1400003@rgu.ac.uk>
2015-11-09 14:31:12 +0000

    Mike test 1

--- Login.php
+++ Login.php
@@ -38,7 +38,7 @@
                             $username = mysqli_escape_string($_POST['username']);
                             $password = mysqli_escape_string($_POST['password']);
                             //$password = md5($password);
-                            $sqlget = mysqli_query ("SELECT * FROM users WHERE Username= '"$username"' AND Password = '"$password"'");
+                            $sqlget = mysqli_query ("SELECT * FROM users WHERE Username= '$username' AND Password = '$password'");
                             if (mysqli_num_rows($sqlget) > 0) {
                                 echo "you are now logged in.";
                             } else {

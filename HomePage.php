98ca8a3eb925453722935be89ac26ddc17ee80f3
Roronoa <wld-ice@hotamil.com>
2015-12-21 17:34:14 +0000

    12/17/2015

--- HomePage.php
+++ HomePage.php
@@ -34,13 +34,14 @@
             $password = md5($password);
             $rule = "SELECT role FROM users WHERE username ='$username'";
             $run_rule = $db->query($rule);
+            WHILE ($row = $run_rule->fetch_array());
             $sqlget = "SELECT * FROM users WHERE username ='$username'
                             AND password ='$password'";
             $run_user = mysqli_query($db, $sqlget);
             $check_user= mysqli_num_rows($run_user);
             if($check_user==1){
                 $_SESSION['username']=$username;
-                echo 'Welcome ' . $_SESSION['username'] . ' You are ' .$run_rule;
+                echo 'Welcome ' . $_SESSION['username'] . ' You are ' .$row;
             } else {
                 //header('Refresh:5; url=Login.php');
                 echo "<script>alert('Please Login'); location.href='Login.php';</script>";

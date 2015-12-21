6249f5ce9fafc95fe2fa470f17aea7db10d1da36
Roronoa <wld-ice@hotamil.com>
2015-12-21 20:43:01 +0000

    12/17/2015

--- HomePage.php
+++ HomePage.php
@@ -24,6 +24,9 @@
     <div class="Container page">
         <h1>
         <?php
+        ini_set('display_errors', 1);
+        ini_set('display_startup_errors', 1);
+        error_reporting(E_ALL);
          require('config.php');
 
          if (isset($_POST['Login'])) {

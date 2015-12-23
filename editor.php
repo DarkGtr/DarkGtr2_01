6b6731d802d1a7e3accc7b3d7c5bedec4e1d4897
Roronoa <wld-ice@hotamil.com>
2015-12-23 00:52:12 +0000

    12/17/2015

---
+++ editor.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>index</title>
+    <link rel="stylesheet" type="text/css" href="DarkGtr2.css">
+    <link rel="shortcut icon" href="#">
+</head>
+<body>
+<div ID = "MainHeader">
+    <div class="Container">
+        <div ID="LogoArea">
+            <a href="index.html" ID="LogoLink">Logo</a>
+        </div>
+        <div ID="NavArea">
+            <ul ID="Nav">
+                <a href="Logout.php"><li>Logout</li></a>
+            </ul>
+        </div>
+    </div>
+</div>
+<div ID="MainArea">
+    <div class="Container page">
+        <h1> Editing </h1>
+    </div>
+</div>
+<div ID="footer">
+    <div class="Container">
+        Contact: 1400003@rgu.ac.uk
+    </div>
+</div>
+</body>

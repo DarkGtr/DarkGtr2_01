39b8f91836c28c74bc82916bad0e1e59dcb43978
Roronoa <wld-ice@hotamil.com>
2015-12-24 19:34:36 +0000

    12/17/2015

--- admin.php
+++ admin.php
@@ -43,7 +43,7 @@
     <div class="Container page">
         <h1> <?php echo "Admin: " . $_SESSION['username']; ?> </h1>
         <h2>
-            <a href="admin.php?level=role">Users_level</a> |
+            <a href="admin.php?level=update">Users_level</a> |
             <a href="admin.php?user=delete">Users_delete</a>
         </h2>
         <h3>

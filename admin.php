ae9e197c4dde00b89ed7623fedbf61dc082496e6
Roronoa <wld-ice@hotamil.com>
2015-12-24 01:23:51 +0000

    12/17/2015

--- admin.php
+++ admin.php
@@ -3,15 +3,20 @@
     if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
         echo "<script>alert('Please Login'); location.href='Login.php';</script>";
     }
+?>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require('config.php');
 
-    $role = "SELECT * FROM users WHERE username ='{$_SESSION['username']}'";
-    $run_role = $db->query($role);
-    WHILE ($row = $run_role->fetch_array())
+$role = "SELECT * FROM users WHERE username ='{$_SESSION['username']}'";
+$run_role = $db->query($role);
+WHILE ($row = $run_role->fetch_array())
     $row['role'];
-    if ($row['role'] != 'Admin') {
-        echo ("error");
-    }
-
+if ($row['role'] != 'Admin') {
+    echo ("error");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">

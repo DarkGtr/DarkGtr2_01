0b6827199644fd41a29f0ea214679682cc374dfa
Roronoa <wld-ice@hotamil.com>
2015-12-24 20:37:20 +0000

    12/17/2015

--- update.php
+++ update.php
@@ -4,12 +4,18 @@
     error_reporting(E_ALL);
     require('config.php');
 
-    $update_id = $_GET['update'];
-    $updateR = $_GET['updateR'];
-    $up_query = "UPDATE 'role' SET '$update_id' WHERE 'username'='$u_username2'";
-    $run_query2 = $db->query($up_query);
+    if(isset($_POST['submit'])) {
 
-    if ($run_query2) {
-        echo "<script>alert('Updated!'); location.href='admin.php?level=role';</script>";
+        $update_id = $_POST['update'];
+        $updateR = $_POST['submit'];
+
+        $up_query = "UPDATE users SET role='$update_id' WHERE 'username'='$u_username'";
+        $run_query2 = $db->query($up_query);
+
+        if ($run_query2) {
+            echo "<script>alert('Updated!'); location.href='admin.php?level=role';</script>";
+        }
     }
+
+
 ?>

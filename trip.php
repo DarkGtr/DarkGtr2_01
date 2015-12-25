b0dc492404b32f2359c014464a211ce01b34abf5
Roronoa <wld-ice@hotamil.com>
2015-12-25 12:10:19 +0000

    12/17/2015

--- trip.php
+++ trip.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
-    echo "<script>alert('Please Login'); location.href='Login.php';</script>";
 }
 
 ?>

ad2f12d7e9d498e41992c55990e66d2c9a27a6db
Roronoa <wld-ice@hotamil.com>
2015-12-25 02:39:30 +0000

    12/17/2015

--- authors.php
+++ authors.php
@@ -62,7 +62,7 @@
         while($row = $run_query->fetch_array()){
             $u_username = $row['username'];
 
-            echo "<br>" . "<h2>$u_username</h2>";} ?>"></a>
+            echo "<br>" . "<h2>$u_username</h2>";} ?>">$u_username</a>
 
 
     </div>

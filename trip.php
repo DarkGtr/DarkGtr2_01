28c5e75b97a3f1d7d02128dff1d585d4cedb4de9
Roronoa <wld-ice@hotamil.com>
2015-12-25 13:52:51 +0000

    12/17/2015

--- trip.php
+++ trip.php
@@ -94,7 +94,7 @@ WHILE ($row = $run_role->fetch_array()) {
                                 $query = "INSERT INTO comments (post_id, username, comment)
                             VALUES ('$p_id', '{$_SESSION['username']}'), '$comment'";
                                 if (mysqli_query($db, $query)) {
-                                    echo "<script>alert('You have just commented!');</script>";
+                                    echo "<script>alert('You have just commented!');location.href='trip.php?trip='$post_title'</script>";
                                 } elseif ($comment = '') {
                                    echo "<script>alert('Enter a comment');</script>";
                             }

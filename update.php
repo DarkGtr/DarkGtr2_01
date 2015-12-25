d198439b0cef86fead0aadf0557fec4c2e80fb31
Roronoa <wld-ice@hotamil.com>
2015-12-25 03:46:40 +0000

    12/17/2015

--- update.php
+++ update.php
@@ -10,10 +10,9 @@
         $updateR = $_GET['submit'];
 
         $up_query = "UPDATE users SET role='$update_id' WHERE username='$updateR'";
-        $run_query = $db->query($up_query);
-        echo $update_id . $updateR;
 
-        if (mysqli_query($db, $run_query)) {
+
+        if (mysqli_query($db, $up_query)) {
             echo "<script>alert('Updated!'); location.href='admin.php?level=update';</script>";
         } else {
             echo "<script>alert('Something went wrong!'); location.href='admin.php?level=update';</script>";

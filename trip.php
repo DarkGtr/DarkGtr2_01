fe9a92360beadace810d0574b9105fb2e377a889
Roronoa <wld-ice@hotamil.com>
2015-12-25 12:15:11 +0000

    12/17/2015

--- trip.php
+++ trip.php
@@ -43,8 +43,8 @@ WHILE ($row = $run_role->fetch_array()) {
             <a href="index.html" ID="LogoLink">Logo</a>
         </div>
         <div ID="NavArea">
-            <ul ID="Nav">
-                <a href="Logout.php"><li>Logout</li></a>
+            <ul ID="Nav"><?php if ($_SESSION['username'] == true){?>
+                <a href="Logout.php"><li>Logout</li></a> <?php } ?>
                 <a href="HomePage.php"><li>Home</li></a>
             </ul>
         </div>

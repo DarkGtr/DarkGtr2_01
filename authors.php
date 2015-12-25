600fee4a7931046f402d138898e9b9231c008051
Roronoa <wld-ice@hotamil.com>
2015-12-25 02:46:31 +0000

    12/17/2015

--- authors.php
+++ authors.php
@@ -61,7 +61,7 @@
         $run_query = $db->query($list_query);
         while($row = $run_query->fetch_array()){
             $u_username = $row['username'];
-            echo "<br>" . $u_username;
+            echo $u_username;
         ?>"><h2><?php echo "<br>" . $u_username;} ?></h2></a>
 
 

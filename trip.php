9b20ed1c97a55ab51d09fa429c1ae2394f163b4e
Roronoa <wld-ice@hotamil.com>
2015-12-25 13:58:29 +0000

    12/17/2015

--- trip.php
+++ trip.php
@@ -2,13 +2,13 @@
 session_start();
 if (!(isset($_SESSION['username']) && $_SESSION['username'] != '')) {
 }
-
+require('config.php');
 ?>
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-require('config.php');
+
 
 $role = "SELECT * FROM users WHERE username ='{$_SESSION['username']}'";
 $run_role = $db->query($role);

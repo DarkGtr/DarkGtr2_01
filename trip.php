73e66839ce1133046b651a51dc3c85d4ef80f8f1
Roronoa <wld-ice@hotamil.com>
2015-12-25 11:48:20 +0000

    12/17/2015

--- trip.php
+++ trip.php
@@ -55,22 +55,23 @@ WHILE ($row = $run_role->fetch_array()) {
     <div class="Container page">
         <h2>
         <?php
+            if($_GET['trip']){
              $trip = $_GET['trip'];
             $query = "SELECT * FROM post WHERE p_title='$trip'";
             $run_query = $db->query($query);
             while($row = $run_query->fetch_array()) {
-                $post_title = $_POST['p_title'];
-                $post_country = $_POST['p_country'];
+                $post_username = $_POST['username'];
                 $post_date = $_POST['p_date'];
-                $post_Author = $_POST['username'];
-                $post_hashtags = $_POST['p_tags'];
+                $post_title = $_POST['p_title'];
                 $post_content = $_POST['trip_info'];
-                $post_image = $_FILES['image'];
+                $post_country = $_POST['p_country'];
+                $post_hashtags = $_POST['p_tag'];
+
         ?>
             <h2><?php echo $post_title . "by" . $post_Author . "Date: " . $post_date; ?></h2><hr>
             <h2><?php echo $post_content; ?></h2>
-            <h2>Post Country: <?php $_POST['p_country']; ?></h2>
-            <h2>Tags: <?php $_POST['p_tags']; }?></h2></h2>
+            <h2>Post Country: <?php echo $post_country; ?></h2>
+            <h2>Tags: <?php echo $post_hashtags; }}?></h2></h2>
     </div>
 </div>
 <div ID="footer">

64e3cefa4a65638e7b2e4ecaaafe098285164512
Roronoa <wld-ice@hotamil.com>
2015-12-25 07:49:47 +0000

    12/17/2015

--- authorpage.php
+++ authorpage.php
@@ -60,7 +60,7 @@ WHILE ($row = $run_role->fetch_array()) {
         ?>
         <h1>Author name: <?php echo $author; ?></h1>
         <h2>Authors posts:</h2>
-        <h3><a href="trip.php?trip=<?php echo $post_title; ?>"><?php echo $post_title . '<br>';}?></a></h3>
+        <h3><a href="trip.php?trip=<?php echo $post_title; ?>"><?php echo $post_title . "<br>";}?></a></h3>
     </div>
 </div>
 <div ID="footer">

9736baba60b5e2c801817486be004c733b6456f9
Roronoa <wld-ice@hotamil.com>
2015-12-25 04:06:31 +0000

    12/17/2015

--- admin.php
+++ admin.php
@@ -96,7 +96,7 @@
                                 <option value="Reader">Reader</option>
                                 <option value="Author">Author</option>
                                 <option value="Admin">Admin</option>
-                            <input name="Submit" type="submit" value="<?php echo $u_username; ?>"/>
+                            <input name="submit" type="submit" value="<?php echo $u_username; ?>"/>
                             </select>
                         </form>
                     </td>
